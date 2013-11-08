f0ce621e40aacfdb77343167da8b1faecd61ef23
vincent.sue <gnux123@gmail.com>
2013-11-08 14:30:37 +0800

    1.add remote. 2.angular two way binding test.

--- app/js/controller.js
+++ app/js/controller.js
@@ -6,13 +6,13 @@ angular.module('umallApp').config(function($stateProvider, $urlRouterProvider) {
         url: '/prods', 
         views: {
             'main': {
-                templateUrl: 'view/prods.html'
-                //controller:  'LoginCtrl',
+                templateUrl: 'view/prods.html',
+                controller:  'prodCtrl',
             }
         }
     })
     .state('shop', {
-        url: '/shopCart/:steps/:message', 
+        url: '/shopCart/:steps/:name/:price', 
         views: {
             'main': {
                 templateUrl: 'view/shopcart.html',
@@ -22,14 +22,31 @@ angular.module('umallApp').config(function($stateProvider, $urlRouterProvider) {
     })
 });
 
-//shopCart
+//prodsController
+var prodCtrl = function($scope, $stateParams){
+    $scope.prods = {
+        name: '六圓環生技特級納豆營養素六盒', 
+        subdes: '閃購模式又稱限時搶購模式，起源於法國網站Vente Privée。閃購模式即是以互聯網為媒介的B2C電子零售交易活動，以限時特賣的形式，定期定時推出國際知名品牌的商品，一般以原價1-5折的價格供專屬會員限時搶購，每次特賣時間持續5-10天不等',
+        price: 400000000
+    };
+
+    //$scope.prods.price = $stateParams.price;
+    //console.log($scope.prods.price);
+
+}
+
+//shopCartController
 var shopCtrl = function($scope, $stateParams){
     $scope.steps = $stateParams.steps;
-    $scope.message = $stateParams.message;
+    $scope.name = $stateParams.name;
+    $scope.price = $stateParams.price;
     // console.log($stateParams.steps);
-    if($scope.steps == 1){
-     alert($scope.steps + "," + $scope.message);
-    }
+    // if($scope.steps == 1){
+    //  alert($scope.steps + "," + $scope.message);
+    // }
+
+    //alert($scope.name + "," + $scope.price);
 }
 
 umallApp.controller('shopCtrl',shopCtrl);
+umallApp.controller('prodCtrl',prodCtrl);
