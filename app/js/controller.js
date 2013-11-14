8eefc194ec7c638466aeb32380c90d094df29573
vincent.sue <gnux123@gmail.com>
2013-11-14 14:23:22 +0800

    1.ng-animate效果設計及測試.

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,25 +1,17 @@
-var umallApp = angular.module('umallApp',['ui.router']);
+var umallApp = angular.module('umallApp',['ui.router','ngAnimate']);
 //ui-router
 angular.module('umallApp').config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/prods");  
     $stateProvider
     .state('prods', {
         url: '/prods', 
-        views: {
-            'main': {
-                templateUrl: 'view/prods.html',
-                controller:  'prodCtrl',
-            }
-        }
+        templateUrl: 'view/prods.html',
+        controller:  'prodCtrl'
     })
     .state('shop', {
         url: '/shopCart/:steps/', 
-        views: {
-            'main': {
-                templateUrl: 'view/shopcart.html',
-                controller:  'shopCtrl'
-            }
-        }
+        templateUrl: 'view/shopcart.html',
+        controller:  'shopCtrl'
     })
 });
 
@@ -27,6 +19,8 @@ angular.module('umallApp').config(function($stateProvider, $urlRouterProvider) {
 //factory
 umallApp.factory('Data', function(){
     return [{
+        //prodData
+        nums: '901122345',
         img: 'http://img2.u-mall.com.tw/UserFiles/Product/00470/00470304/26ad40e9-227c-44b1-acde-268123ce3c64.jpg',     
         name: '六圓環生技特級納豆營養素六盒', 
         subdes: '閃購模式又稱限時搶購模式，起源於法國網站Vente Privée。閃購模式即是以互聯網為媒介的B2C電子零售交易活動，以限時特賣的形式，定期定時推出國際知名品牌的商品，一般以原價1-5折的價格供專屬會員限時搶購，每次特賣時間持續5-10天不等',
@@ -37,9 +31,11 @@ umallApp.factory('Data', function(){
         piece: 1
     },
     {
+        //userData
         name: "蘇靖文",
         sex: "男",
-        mobile: "0987-797768"
+        mobile: "0987-797768",
+        eCoupon: ["100","200","400","500"]
     }];
 });
 
@@ -59,10 +55,10 @@ var shopCtrl = function($scope, Data ,$stateParams){
 
 
     var update = function() {
-        $scope.counter++;
+        //$scope.counter++;
         Data[0].price = prods.price;
         Data[0].piece = prods.piece;
-        alert($scope.counter);
+        //alert($scope.counter);
     };
 }
 
