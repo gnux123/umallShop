1d29c36c6a1d0556c9a631b8e1362e1a904c70b1
vincent.sue <gnux123@gmail.com>
2013-11-29 15:36:56 +0800

    1. 復原輪盤遊戲demo用. 2. 調整輪盤js遊戲.

--- app/js/controller.js
+++ app/js/controller.js
@@ -112,6 +112,49 @@ var gameCtrl = function($scope, Data ,$stateParams){
     $scope.change = function(){
         $scope.gameImg = "game_1";
     }
+
+   Zepto(function($){
+
+        sizefix();
+        $(window).resize(function(){sizefix();});
+
+
+
+        // var $value = 8,
+        //     _deg = ($value*45)*-1;
+        // $.fn.addKeyframe([{
+        //     name: "board-main-run",
+        //     "from":$.keyframe.browserCode()+"transform:rotate(0deg)",
+        //     "to":$.keyframe.browserCode()+"transform:rotate("+(1800+_deg)+"deg)",
+        // }]);
+
+
+        $("a.blackCircle").on('click',function(){
+            // alert(1);
+            $(".main-animate").find(".wrap, .lightBall").removeClass("linear");
+            $(".main-animate").find(".lightBall").addClass("lightBall-run");
+            // $(".main-animate").find(".wrap").playKeyframe({
+            //     name: 'board-main-run',
+            //     timingFunction:'ease',
+            //     repeat:'1',
+            //     duration:'8000',
+            //     direction:'normal',
+            //     fillMode: 'forwards',
+            //     delay: 0
+            // });
+            //$(".main-animate").find(".blackCircle").addClass("blackCircle-run");
+        });
+
+        function sizefix(){
+            var _h = $(window).height(),
+                _navH = $(".navs").height();
+            $(".main, .gameEventr").height(_h - _navH - 20);
+        }
+    })
+
+
+
+
 }
 
 umallApp.controller('shopCtrl',shopCtrl);
