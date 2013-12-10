2764c2004a2fbd662107e479c9b7463a97dcac3c
vincent.sue <gnux123@gmail.com>
2013-12-10 11:01:44 +0800

    1. 修改首頁ad風格調整. 2. 調整拉長商品評論呈現.

--- app/js/control/gameCtrl.js
+++ app/js/control/gameCtrl.js
@@ -3,34 +3,30 @@
 var gameCtrl = function($scope, Data ,$stateParams){
     $scope.gameImg = "game_0";
     $scope.change = function(){
-        $scope.gameImg = "game_1";
+        Zepto(function($){
+            setTimeout(function(){
+                $(".popup_2").css("display","block");
+            },1000);              
+        });
+        //$scope.gameImg = "game_1";
     }
 
     window.addEventListener('shake', shakeEventDidOccur, false);
     
     //define a custom method to fire when shake occurs.
     function shakeEventDidOccur () {
-    
-        // //put your own code here etc.
-        // if (confirm("Undo?")) {
-
-        // }
         Zepto(function($){
-            $(".main-animate").find(".wrap, .lightBall").removeClass("linear");
-            $(".main-animate").find(".lightBall").addClass("lightBall-run");
-            // $(".main-animate").find(".wrap").playKeyframe({
-            //     name: 'board-main-run',
-            //     timingFunction:'ease',
-            //     repeat:'1',
-            //     duration:'8000',
-            //     direction:'normal',
-            //     fillMode: 'forwards',
-            //     delay: 0
-            // });
-            //$(".main-animate").find(".blackCircle").addClass("blackCircle-run");
-            setTimeout(function(){
-                $(".popup").css("display","block");
-            },7000);        
+            if($stateParams.pages == 1){
+                $(".main-animate").find(".wrap, .lightBall").removeClass("linear");
+                $(".main-animate").find(".lightBall").addClass("lightBall-run");
+               setTimeout(function(){
+                    $(".popup").css("display","block");
+                },7000); 
+            }else{
+                setTimeout(function(){
+                    $(".popup_2").css("display","block");
+                },1000);
+            }       
         });
     }
 
@@ -72,7 +68,7 @@ var gameCtrl = function($scope, Data ,$stateParams){
         function sizefix(){
             var _h = $(window).height(),
                 _navH = $(".navs").height();
-            $(".gameEventr").height(_h - _navH - 20);
+            $(".gameEvent").height(_h - _navH - 20);
         }
     })
 }
